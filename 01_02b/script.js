226173f396eaad3736917077cf5aa7e55a63a93b
Cristina R.M. <116902088+C-RM22@users.noreply.github.com>
2025-06-04 14:01:41 +0000

    Fixed code, wrong file

--- 01_02b/script.js
+++ 01_02b/script.js
@@ -8,25 +8,34 @@ import data from "./data.js";
 
 const mainContent = document.querySelector(".main-content");
 
+const buildImage = (imgData) => {
+  let srcset = `${imgData.urls.full} ${imgData.width}w`;
+  if (imgData.urls.regular) {
+    srcset = srcset + `, ${imgData.urls.regular} 1080w`;
+  }
+  if (imgData.urls.small) {
+    srcset = srcset + `, ${imgData.urls.small} 400w`;
+  }
+  const img = `
+  <img
+    srcset="${srcset}"
+    sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
+    src="${imgData.urls.regular}"
+    width="${imgData.width}"
+    height="${imgData.height}"
+    alt="${imgData.description}"
+    loading="lazy"
+  />`;
+  return img;
+};
+
 const Card = (data) => {
   const imgData = data[0];
   const date = new Date(imgData.created_at);
 
   const markup = `
     <figure class="image">
-      <img
-        srcset="
-          ${imgData.urls.full} ${imgData.width}w,
-          ${imgData.urls.regular} 1080w,
-          ${imgData.urls.small} 400w
-        "
-        sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
-        src="${imgData.urls.regular}"
-        width="${imgData.width}"
-        height="${imgData.height}"
-        alt="${imgData.description}"
-        loading="lazy"
-      />
+    ${buildImage(imgData)}
       <figcaption class="image__caption">
         <h3 class="image__title">${imgData.description}</h3>
         <div class="image__meta">
